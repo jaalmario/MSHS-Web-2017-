6654addd69b5cd08aea551991b3f790b9e7f4a7b
Jovannie Almario <59501933+jaalmario@users.noreply.github.com>
2022-05-13 18:49:41 +0800

    Add files via upload

---
+++ MSHS site/js/home.js
@@ -0,0 +1,291 @@
+
+	function hov1() {
+		var t=document.getElementById("ts");
+	var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+    	i.style.marginLeft = "0px";
+    	la.style.display = 'none';
+    	ibon.style.display = 'none';
+}
+	function hov2() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+    	i.style.marginLeft = '-100%';
+    	la.style.display = 'none';
+    	ibon.style.display = 'none';
+}
+	function hov3() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+    i.style.marginLeft = '-200%';
+    	la.style.display = 'none'; 
+    	ibon.style.display = 'none';
+}
+	function hov4() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+   		i.style.marginLeft = '-300%';
+    	la.style.display = 'none'; 
+    	ibon.style.display = 'none';
+	}	
+
+	function hov5() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info"); 
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+    	i.style.marginLeft = '-400%';
+		la.style.display = 'none';
+    	ibon.style.display = 'none';
+	}
+	function hov6() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+   		i.style.marginLeft = '-500%';
+    	la.style.display = 'none';
+    	ibon.style.display = 'none';
+	}	
+
+	function hov7() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+   		i.style.marginLeft = '-600%';
+    	la.style.display = 'none';
+    	ibon.style.display = 'none';
+	}		
+
+	function hov8() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+   		i.style.marginLeft = '-700%';
+    	la.style.display = 'none';
+    	ibon.style.display = 'none';
+	}	
+
+	function hov9() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+   		i.style.marginLeft = '-800%';
+    	la.style.display = 'none';
+    	ibon.style.display = 'none';	}
+
+	function hov10() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'inline-block';
+   		i.style.marginLeft = '-900%';
+    	la.style.display = 'none';
+    	ibon.style.display = 'none';
+	}
+
+
+	function hov11() {
+		var t=document.getElementById("ts");
+		var i=document.getElementById("info");
+		var la=document.getElementById("french");
+		var ibon=document.getElementById("list");
+		t.style.display = 'none';
+   		i.style.marginLeft = '-600%';
+    	la.style.display = 'inline-block';
+    	ibon.style.display = 'inline-block';
+	}
+
+
+
+	function data1(){
+		var m=document.getElementById("info");
+		var s=document.getElementById("hot");
+		var h=document.getElementById("sel");
+		var s1=document.getElementById("sele");
+		var i=document.getElementById("p1");
+		var g=document.getElementById("p2");
+		var l=document.getElementById("p3");
+		m.style.marginLeft = '-100%';
+		s.style.width = '32.5%';
+		s.style.height = '50%';
+		s.style.fontSize = '22px';
+		s.style.position ='absolute';
+		s.style.top ='auto';
+		s.style.right ='0';
+		s.style.bottom ='0';
+		s.style.left ='auto';
+		h.style.width ='65%';
+		h.style.position ='absolute';
+		h.style.top ='0';
+		h.style.right ='auto';
+		h.style.bottom ='auto';
+		h.style.left ='0';
+		h.style.height ='100%';
+		s1.style.position ='absolute';
+		s1.style.top ='0';
+		s1.style.right ='0';
+		s1.style.left ='auto';
+		s1.style.bottom ='auto';
+		s1.style.width ='32.5%';
+		s1.style.height ='50%';
+		i.style.fontSize = '22px';
+		i.style.height = '60px';
+		i.style.bottom = '65px';
+		g.style.fontSize = '30px';
+		g.style.height = '75px';
+		g.style.bottom = '80px';
+		l.style.fontSize = '22px';
+		l.style.height = '60px';
+		l.style.bottom = '65px';
+	}
+
+
+	function data2(){
+		var m=document.getElementById("info");
+		var s=document.getElementById("hot");
+		var h=document.getElementById("sel");
+		var s1=document.getElementById("sele");
+		var i=document.getElementById("p1");
+		var g=document.getElementById("p2");
+		var l=document.getElementById("p3");
+		m.style.marginLeft = '-200%';
+		s.style.width = '32.5%';
+		s.style.position ='absolute';
+		s.style.top ='0';
+		s.style.right ='0';
+		s.style.bottom ='auto';
+		s.style.left ='auto';
+		s.style.height = '50%';
+		s1.style.width ='65%';
+		s.style.fontSize = '22px';
+		h.style.width ='32.5%';
+		h.style.height ='50%';
+		h.style.position ='absolute';
+		h.style.right ='0';
+		h.style.bottom ='0';
+		h.style.top ='auto';
+		h.style.left ='auto';
+		s1.style.position ='absolute';
+		s1.style.left ='0';
+		s1.style.top ='0';
+		s1.style.right ='auto';
+		s1.style.bottom ='auto';
+		s1.style.height ='100%';
+		i.style.fontSize = '22px';
+		i.style.height = '60px';
+		i.style.bottom = '65px';
+		g.style.fontSize = '22px';
+		g.style.height = '60px';
+		g.style.bottom = '65px';
+		l.style.fontSize = '30px';
+		l.style.height = '75px';
+		l.style.bottom = '80px';
+	}
+
+	function data(){
+		var m=document.getElementById("info");
+		var s=document.getElementById("hot");
+		var h=document.getElementById("sel");
+		var s1=document.getElementById("sele");
+		var i=document.getElementById("p1");
+		var g=document.getElementById("p2");
+		var l=document.getElementById("p3");
+		m.style.marginLeft = '0%';
+		s.style.width = '65%';
+		s.style.position ='absolute';
+		s.style.top ='0';
+		s.style.right ='auto';
+		s.style.bottom ='auto';
+		s.style.left ='0';
+		s.style.height = '100%';
+		h.style.width ='32.5%';
+		h.style.height ='50%';
+		h.style.position ='absolute';
+		h.style.top ='0';
+		h.style.right ='0';
+		h.style.bottom ='auto';
+		h.style.left ='auto';
+		s1.style.width ='32.5%';
+		s1.style.position ='absolute';
+		s1.style.top ='auto';
+		s1.style.right ='0';
+		s1.style.bottom ='0';
+		s1.style.left ='auto';
+		s1.style.height ='50%';
+		i.style.fontSize = '30px';
+		i.style.height = '75px';
+		i.style.bottom = '80px';
+		g.style.fontSize = '22px';
+		g.style.height = '60px';
+		g.style.bottom = '65px';
+		l.style.fontSize = '22px';
+		l.style.height = '60px';
+		l.style.bottom = '65px';
+	}
+
+
+
+
+	function click0() {
+		var i=document.getElementById("info");
+		var principal=document.getElementById("principal");  
+		var la=document.getElementById("french");
+    	i.style.marginLeft = '0';
+    	principal.style.display = 'none'; 
+    	la.style.display = 'none';
+
+}
+	function click1() {
+		var i=document.getElementById("info");
+		var principal=document.getElementById("principal");  
+		var la=document.getElementById("french");
+    	i.style.marginLeft = '-100%';
+    	principal.style.display = 'none'; 
+    	la.style.display = 'none';
+}
+	function click2() {
+		var i=document.getElementById("info");
+		var principal=document.getElementById("principal");  
+		var la=document.getElementById("french");
+    i.style.marginLeft = '-200%';
+    	principal.style.display = 'none';
+    	la.style.display = 'none'; 
+}
+	function click3() {
+		var i=document.getElementById("info");
+		var principal=document.getElementById("principal");  
+		var la=document.getElementById("french");
+   		i.style.marginLeft = '-300%';
+    	principal.style.display = 'none';
+    	la.style.display = 'none'; 
+	}	
+
+	function click4() {
+		var i=document.getElementById("info"); 
+		var principal=document.getElementById("principal");  
+		var la=document.getElementById("french");
+    	i.style.marginLeft = '-400%';
+    	principal.style.display = 'inline-block'; 
+		la.style.display = 'none';
+	}
